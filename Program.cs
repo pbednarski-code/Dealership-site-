f852bb870e9df983b540642a2cf1695aa3797aee
pbednarski-code <pawel.bednarski100@gmail.com>
2026-05-26 11:48:24 +0200

    Edited

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using DealerAutoMVC.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDistributedMemoryCache();
